49536f114b287d659c8806ada4c460b47205b4a5
unknown <nacuandrei10@gmail.com>
2023-12-14 16:20:16 +0200

    Added profile picture, username, title and list of personal poems

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const ProfilePage());
-}
+// void main() {
+//   runApp(const ProfilePage());
+// }
 
 class ProfilePage extends StatefulWidget {
-  const ProfilePage({super.key});
+  const ProfilePage({Key? key}) : super(key: key);
 
   @override
   State<ProfilePage> createState() => _ProfilePageState();
@@ -16,8 +16,39 @@ class _ProfilePageState extends State<ProfilePage> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Profile page"),
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                const ClipOval(
+                  child: Image(
+                    image: NetworkImage(
+                        "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dc/Eminescu.jpg/330px-Eminescu.jpg"),
+                    width: 100,
+                    fit: BoxFit.contain,
+                  ),
+                ),
+                const SizedBox(height: 24.0),
+                Text(
+                  "Unknown",
+                  style: Theme.of(context).textTheme.headline5,
+                ),
+                const SizedBox(height: 8.0),
+                Text(
+                  "-Poet-",
+                  style: Theme.of(context).textTheme.subtitle2,
+                ),
+                const SizedBox(height: 32.0),
+                Text(
+                  "Poems",
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+                const SizedBox(height: 8.0),
+              ],
+            ),
+          ),
         ),
       ),
     );
