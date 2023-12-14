64257d7980bef846ac703eb052fcd74a69b94699
unknown <nacuandrei10@gmail.com>
2023-12-14 15:58:40 +0200

    Added template profile

---
+++ lib/profile.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ProfilePage());
+}
+
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({super.key});
+
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Profile page"),
+        ),
+      ),
+    );
+  }
+}
