6cfaf0ca36d2831c2146ec066cc3eea9f1ab7b67
unknown <nacuandrei10@gmail.com>
2024-01-09 11:19:40 +0200

    Initial commit, added favourites

--- lib/favourites.dart
+++ lib/favourites.dart
@@ -2,20 +2,67 @@ import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:poetry_app/Compose/compose.dart';
 import 'package:poetry_app/profile.dart';
+import 'package:poetry_app/home.dart';
 
-import 'favourites.dart';
-
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
+class PoemList extends StatefulWidget {
+  const PoemList({super.key});
   @override
-  State<HomePage> createState() => _HomePageState();
+  State<PoemList> createState() => _PoemListState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _PoemListState extends State<PoemList> {
+  List<String> _poems = [
+    '''
+    Roses are red,
+    Violets are blue,
+    Sugar is sweet,
+    And so are you.
+    ''',
+    '''
+    The world is full of magic things,
+    Patiently waiting for our senses to grow sharper.
+    ''',
+    '''
+    The road to success is long and winding,
+    But with hard work and determination,
+    You will surely reach your destination.
+    ''',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: const Text('Favourites'),
+      ),
+      body: ListView.builder(
+        itemCount: _poems.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  Text(_poems[index]),
+                  const SizedBox(height: 8),
+                  // RaisedButton(
+                  //   onPressed: () {
+                  //     Navigator.push(
+                  //       context,
+                  //       MaterialPageRoute(
+                  //         builder: (context) => PoemView(poem: _poems[index]),
+                  //       ),
+                  //     );
+                  //   },
+                  //   child: Text('View Full Poem'),
+                  // ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
       bottomNavigationBar: Container(
         color: Colors.black,
         child: Padding(
@@ -72,6 +119,29 @@ class _HomePageState extends State<HomePage> {
                   },
                 ),
               ]),
+        ),
+      ),
+    );
+  }
+}
+
+class PoemView extends StatelessWidget {
+  final String poem;
+
+  const PoemView({super.key, required this.poem});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Poem View'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Text(poem),
+          ],
         ),
       ),
     );
