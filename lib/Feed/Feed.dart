796ee979f7774a53e33290de2b26b4604c049bf7
GrigoreGeorgeAlexandru <61271003+GrigoreGeorgeAlexandru@users.noreply.github.com>
2024-01-15 18:42:20 +0200

    add background image

--- lib/Feed/Feed.dart
+++ lib/Feed/Feed.dart
@@ -1,19 +1,25 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
-import 'package:poetry_app/Data/Models/PublishedPoemModel.dart';
+
 import 'package:poetry_app/Data/Services/DataService.dart';
 import 'package:poetry_app/Feed/PoemList.dart';
-import 'package:poetry_app/Feed/PoemListTile.dart';
 
 class Feed extends StatelessWidget {
   const Feed({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return PoemList(
-      poemsStream: GetIt.instance<DataService>().getPublishedPoems(),
-      published: true,
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          fit: BoxFit.cover,
+          image: AssetImage('assets/images/home_background.png'),
+        ),
+      ),
+      child: PoemList(
+        poemsStream: GetIt.instance<DataService>().getPublishedPoems(),
+        published: true,
+      ),
     );
   }
 }
