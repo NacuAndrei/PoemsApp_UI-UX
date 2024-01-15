9fe319fb5c1486464fc290f8708cb516848c232c
GrigoreGeorgeAlexandru <61271003+GrigoreGeorgeAlexandru@users.noreply.github.com>
2024-01-15 18:17:30 +0200

    test commit 123

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/binamircea/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/binamircea/Documents/GitHub/proiect-inginerie-software-poetry-club"
+export "FLUTTER_ROOT=D:\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\projects\test\Poems_App"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
