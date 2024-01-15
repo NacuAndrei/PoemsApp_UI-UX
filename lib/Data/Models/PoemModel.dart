1cdd843399d3e68a4ebfc8ffd18e11b98fcf01f9
GrigoreGeorgeAlexandru <61271003+GrigoreGeorgeAlexandru@users.noreply.github.com>
2024-01-15 18:51:29 +0200

    add isPublished property

--- lib/Data/Models/PoemModel.dart
+++ lib/Data/Models/PoemModel.dart
@@ -2,15 +2,25 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class PoemModel {
   PoemModel(
-      {this.id, required this.title, required this.content, this.photoURL});
+      {this.id,
+      required this.title,
+      required this.content,
+      this.photoURL,
+      this.isPublished = false});
 
   final String? id;
   final String title;
   final String content;
   String? photoURL;
+  bool isPublished;
 
   Map<String, dynamic> toMap() {
-    return {'title': title, 'content': content, 'photoURL': photoURL};
+    return {
+      'title': title,
+      'content': content,
+      'photoURL': photoURL,
+      'isPublished': isPublished
+    };
   }
 
   PoemModel.fromDocumentSnapshot(
@@ -18,11 +28,13 @@ class PoemModel {
       : id = doc.id,
         title = doc.data()["title"],
         content = doc.data()["content"] ?? "",
-        photoURL = doc.data()["photoURL"];
+        photoURL = doc.data()["photoURL"],
+        isPublished = doc.data()["isPublished"] ?? false;
 
   PoemModel.fromMap(Map<String, dynamic> map)
       : id = map["id"],
         title = map["title"],
         content = map["content"] ?? "",
-        photoURL = map["photoURL"];
+        photoURL = map["photoURL"],
+        isPublished = map["isPublished"] ?? false;
 }
